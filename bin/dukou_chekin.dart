e9248b06edd3bd2d6c0d75765c805e224f2a0055
code638 <62542542+code638@users.noreply.github.com>
2024-09-07 23:12:22 +0800

    Update dukou_chekin.dart

--- bin/dukou_chekin.dart
+++ bin/dukou_chekin.dart
@@ -68,7 +68,7 @@ Future<CheckinResult> checkin(String token) async {
     headers: {
       'access-token': token,
     },
-  )).get('https://dukouapi.com/api/user/checkin');
+  )).get('https://flzt.top/user/index');
   print(response.data);
   return CheckinResult.fromJson(json.decode(response.data));
 }
